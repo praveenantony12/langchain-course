17d4fb6d0488476cae357b925d7de62d4ce50edd
praveenantony12 <praveenantony12@yahoo.co.in>
2026-03-23 20:16:11 -0400

    Adding tool call

--- langchain_client.py
+++ langchain_client.py
@@ -2,10 +2,11 @@ import asyncio
 from pathlib import Path
 
 from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_openai import ChatOpenAI
-from langgraph.prebuilt import create_react_agent
-from mcp import StdioServerParameters
+from mcp import ClientSession, StdioServerParameters, stdio_client
 
 load_dotenv()
 
@@ -20,7 +21,15 @@ stdio_server_params = StdioServerParameters(
 
 
 async def main():
-    print("Hello langchain MCP client!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            tools = await load_mcp_tools(session)
+            agent = create_agent(llm, tools)
+            result = await agent.ainvoke(
+                {"messages": [HumanMessage(content="What is 54 + 2 * 3?")]}
+            )
+            print(result["messages"][-1].content)
 
 
 if __name__ == "__main__":
